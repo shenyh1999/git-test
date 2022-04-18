b39db76ab355cd2119678fac3adf33a41f9c8114
syh <syh12345@qq.com>
2022-04-18 10:37:40 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hello hot-fix git!");
         System.out.println("master test");
         System.out.println("hello hot-fix git!!");
+        System.out.println("push test!");
     }
 }
