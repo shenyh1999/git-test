8235a2208fd57788964a56323e7c9211df37534a
shenyh1999 <103892933+shenyh1999@users.noreply.github.com>
2022-04-18 11:09:36 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("master test");
         System.out.println("hello hot-fix git!!");
         System.out.println("push test!");
+        System.out.println("pull test!");
     }
 }
